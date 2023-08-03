44cb893ef3d94b6a458e88096a7ccc12bf9aa9fc
ashish oli <oliashish3@gmail.com>
2023-08-03 23:42:05 +0530

    feat: adding to .tasktron directory in users home directory for storing and fetching tasks letter for listing and manipulating it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::path::Path;
+
+use chrono::{DateTime, Utc};
 use clap::{arg, command, Parser, Subcommand};
+use serde::Serialize;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,10 +23,52 @@ pub enum Commands {
     List,
 }
 
-fn main() {
-    let args = Args::parse();
+#[derive(Debug, Serialize)]
+pub struct Task {
+    pub name: String,
+    pub created: DateTime<Utc>,
+    pub completed: bool,
+    pub cancelled: bool,
+}
 
-    for (idx, task) in args.add.iter().enumerate() {
-        println!("{}. {}", idx + 1, task);
+pub struct Tasks {
+    pub tasks: Vec<Task>,
+}
+
+async fn write_tasks_to_home_directory(args: &Args, home_dir: &Path) -> Result<(), anyhow::Error> {
+    let mut tasks: Vec<Task> = Vec::new();
+
+    for task in args.add.iter() {
+        let task = Task {
+            name: task.to_owned(),
+            created: Utc::now(),
+            completed: false,
+            cancelled: false,
+        };
+
+        tasks.push(task)
     }
+
+    // Check if tasktron directory exits or not
+    let file_dir = format!("{}/.tasktron", home_dir.to_str().unwrap());
+    let file_path = Path::new(&file_dir);
+    if !file_path.exists() {
+        std::fs::create_dir_all(file_path)?
+    }
+
+    std::fs::write(
+        format!("{}/.tasktron/tasks.json", home_dir.display()),
+        serde_json::to_string_pretty(&tasks).expect("unable to parse tasks"),
+    )
+    .expect("Unable to write data to desinated path");
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let args = Args::parse();
+    let home_dir = dirs::home_dir().expect("Unable to fetch home directory");
+    let res = write_tasks_to_home_directory(&args, &home_dir).await;
+    res
 }
